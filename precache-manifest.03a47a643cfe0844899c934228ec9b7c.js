75218be276a64a4365e133a172563ec964e63bb3
ozblech <ozblech87@gmail.com>
2020-04-07 21:56:35 +0300

    Updates

---
+++ precache-manifest.03a47a643cfe0844899c934228ec9b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74590533207ad3f98f1ca03ca60e349b",
+    "url": "/smartbrainFaceRecognition/index.html"
+  },
+  {
+    "revision": "9f6a083b1b7bb3a99498",
+    "url": "/smartbrainFaceRecognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "56ce8ca90eafe31224e0",
+    "url": "/smartbrainFaceRecognition/static/css/main.e990d1e8.chunk.css"
+  },
+  {
+    "revision": "9f6a083b1b7bb3a99498",
+    "url": "/smartbrainFaceRecognition/static/js/2.ace761f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrainFaceRecognition/static/js/2.ace761f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ce8ca90eafe31224e0",
+    "url": "/smartbrainFaceRecognition/static/js/main.10eb6c07.chunk.js"
+  },
+  {
+    "revision": "28c354ba58b717718b2f",
+    "url": "/smartbrainFaceRecognition/static/js/runtime-main.81c1e2c6.js"
+  }
+]);
